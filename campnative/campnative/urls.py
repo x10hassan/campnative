e1ad8a3c2717a75719f9f772dd28bc899f672c29
k1-hedayati <k1.hedayati93@gmail.com>
2015-03-21 16:59:32 +0330

    Added index page

--- campnative/campnative/urls.py
+++ campnative/campnative/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
+    (r'^$', TemplateView.as_view(template_name="index.html")),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^campground/', include('campground.urls')),
 )
